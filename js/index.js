aad170909753dae1d08d23e46a9339a8e7893ba9
Juan Martín Guillén <juanmartinguillen@yahoo.com.ar>
2020-06-23 21:23:55 -0300

    PySpark in CEntOS 8

--- js/index.js
+++ js/index.js
@@ -2,52 +2,55 @@
 
 var base_url = "https://jmlxxvi.github.io/";
 
-var article_links = document.querySelectorAll("#articles-list a");
+var url_search = window.location.search;
+if(url_search) { load_post("posts/" + url_search.substr(1)); }
 
+document.addEventListener("click", event => {
+    if(event.target.classList.contains("internal_link")) {
+        event.preventDefault();
+        event.stopPropagation();
+        load_post("posts/" + event.target.search.substr(1));
+    }
+});
+
+var article_links = document.querySelectorAll("#articles-list a");
 var support_text = document.getElementById("support_text");
 var disqus_thread = document.getElementById("disqus_thread");
 var article_body = document.getElementById("article-body");
 var articles_list = document.getElementById("articles-list");
-
 var home_link = document.getElementById("user_meta");
 
 article_links.forEach(function (element) {
-
     element.addEventListener("click", function (event) {
-
         event.preventDefault();
-
         var url = event.srcElement.attributes.href.textContent;
-        // var link_text = event.srcElement.innerHTML;
-
-        fetch(url)
-            .then(function (response) {
-                return response.text();
-            })
-            .then(function (text) {
-                // document.title = link_text;
-                article_body.innerHTML = text;
-                reload_code_syntax_highlighter();
-                article_body.classList.remove("hidden");
-                support_text.classList.remove("hidden");
-                disqus_thread.classList.remove("hidden");
-                articles_list.classList.add("hidden");
-                disqus_reset(url)
-            });
-
+        load_post(url);
     });
-
 });
 
+function load_post(url) {
+    fetch(url)
+    .then(function (response) {
+        return response.text();
+    })
+    .then(function (text) {
+        article_body.innerHTML = text;
+        reload_code_syntax_highlighter();
+        article_body.classList.remove("hidden");
+        support_text.classList.remove("hidden");
+        disqus_thread.classList.remove("hidden");
+        articles_list.classList.add("hidden");
+        disqus_reset(url)
+    });
+    window.history.pushState("", "", "?" + url.replace("posts/", ""));
+}
+
 home_link.addEventListener("click", function (event) {
-
     event.preventDefault();
-
     articles_list.classList.remove("hidden");
     article_body.classList.add("hidden");
     support_text.classList.add("hidden");
     disqus_thread.classList.add("hidden");
-
 });
 
 function reload_code_syntax_highlighter() {
@@ -57,9 +60,7 @@ function reload_code_syntax_highlighter() {
 }
 
 function disqus_reset(url) {
-
     var di = base_url + url;
-
     //https://stackoverflow.com/questions/8944287/disqus-loading-the-same-comments-for-dynamic-pages
     if (typeof DISQUS != 'undefined') {
         DISQUS.reset({
@@ -70,6 +71,5 @@ function disqus_reset(url) {
             }
         });
     }
-
 }
 
